dd2765a028810f66bc9feb148d7095bcd93e7aac
Masbro <wahyuubuntu@gmail.com>
2025-05-03 11:07:38 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,7 +66,7 @@ const Header: React.FC = () => {
             <img 
               src="https://iili.io/3j86Df2.png?w=120&h=40&fit=crop&auto=format" 
                alt="Toko Putra Logo" 
-               className="h-10 w-auto object-contain"
+               className="h-8 w-auto object-contain"
              />
           </Link>
           
