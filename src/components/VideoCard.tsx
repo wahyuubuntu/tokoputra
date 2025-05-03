7cdaf29ec2a51e0499ce1b88622bab8eefa1acb4
Masbro <wahyuubuntu@gmail.com>
2025-05-03 15:11:56 +0700

    Update VideoCard.tsx

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -14,7 +14,7 @@ const VideoCard: React.FC<VideoCardProps> = ({ video }) => {
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
       <div className="aspect-w-16 aspect-h-9">
         <iframe
-          src={`https://www.youtube.com/embed/${youtubeId}`}
+          src={${youtubeId}
           title={title}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
