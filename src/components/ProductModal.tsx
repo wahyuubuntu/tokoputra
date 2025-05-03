a393724e1a2d57dd2f05784469751ad05abfae81
Masbro <wahyuubuntu@gmail.com>
2025-05-03 13:51:36 +0700

    Update ProductModal.tsx

--- src/components/ProductModal.tsx
+++ src/components/ProductModal.tsx
@@ -48,7 +48,7 @@ const ProductModal: React.FC<ProductModalProps> = ({ isOpen, onClose, product })
         <div className="mt-4">
           {/* Product Images Carousel */}
           <div className="relative">
-            <Carousel className="w-full">
+            <Carousel className="w-400">
               <CarouselContent>
                 <CarouselItem>
                   <img 
